bfe608617fd3465cfa12abdd3a9b768017e20744
Peter Hadac <hadac.peter@gmail.com>
2017-03-15 14:49:00 +0100

    Test

--- viro2.sh
+++ viro2.sh
@@ -1,1 +1,13 @@
-####
+#################################
+#
+#################################
+
+echo "Usage: ./viro2.sh <topolologia>"
+$TOPO=$1
+
+
+
+
+#check if dynamips and KVM is running on localhost
+
+#test deployment dynamips router
